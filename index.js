cfb8c6e2f8694ca90d421d5c3cb36e10744ed38a
Viacheslav Panasiuk <laburunt@gmail.com>
2021-10-15 16:44:42 +0300

    final

--- index.js
+++ index.js
@@ -7,7 +7,33 @@ const colors = [
   '#795548',
 ];
 
+const DELAY = 1000;
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+    bodyEl: document.querySelector('body'),
+    startBtn: document.querySelector('.start'),
+    stopBtn: document.querySelector('.stop')
+ }
+
+refs.startBtn.addEventListener('click', startRandom);
+refs.stopBtn.addEventListener('click', stopRandom);
+
+let intervalId = null;
+
+function startRandom(e) {
+    intervalId = setInterval(() => {
+        const randomColor = randomIntegerFromInterval(0, colors.length);
+        refs.bodyEl.style.backgroundColor = colors[randomColor];
+    }, DELAY);
+    e.target.setAttribute("disabled", "");
+};
+
+function stopRandom() {
+  clearInterval(intervalId);
+  refs.startBtn.removeAttribute("disabled");
 };
 
